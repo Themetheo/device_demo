ea07d4c656565adb4b662140f3440d72e06e9fcc
theme.theo <theme.pattauychai@gmail.com>
2025-05-13 22:13:24 +0700

    up date flask api

--- Backend/app.py
+++ Backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, redirect
+from flask import Flask, request, jsonify, redirect, render_template
 from flask_cors import CORS
 from datetime import datetime
 from Config.loader import load_table_url
@@ -8,9 +8,13 @@ from Backend.log_utils import (
     flush_logs_to_google_sheet
 )
 
-app = Flask(__name__)
-CORS(app)  # ✅ เปิด CORS ให้ frontend fetch ได้
+app = Flask(__name__, template_folder="../templates", static_folder="../static")
 
+CORS(app)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 @app.route("/log", methods=["POST"])
 def log_device():
@@ -24,10 +28,8 @@ def log_device():
         add_log_to_buffer(data)
         print(f"📥 LOG: {data}")
         return jsonify({"status": "ok"})
-
     except Exception as e:
         return jsonify({"error": f"Server error: {str(e)}"}), 500
-
 
 @app.route("/get-url/<table_id>")
 def get_url(table_id):
@@ -38,7 +40,6 @@ def get_url(table_id):
     except Exception as e:
         return jsonify({"error": str(e)}), 400
 
-
 @app.route("/redirect/<table_id>")
 def redirect_table(table_id):
     try:
@@ -48,13 +49,11 @@ def redirect_table(table_id):
     except Exception as e:
         return f"❌ ไม่พบโต๊ะ {table_id}: {e}", 404
 
-
 @app.route("/flush")
 def flush_logs():
     flush_logs_to_monthly_csv()
     flush_logs_to_google_sheet()
     return jsonify({"status": "flushed"})
-
 
 if __name__ == "__main__":
     app.run(debug=True)
